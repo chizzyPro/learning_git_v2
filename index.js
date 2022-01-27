063780d77f892f57d6d4e7ef272b7458a6a25b70
promisechizzy <promisechizzy770@gmail.com>
2022-01-28 00:19:09 +0100

    Added a getPost method

---
+++ index.js
@@ -0,0 +1,11 @@
+const posts = [{title:'Post One', body:"This is the body for post one"}, 
+		{title:"Post Two", body:"This is the body for post two"}]
+	
+
+function getPost(posts){
+	posts.forEach((post) => {
+	console.log(`${post.title}: ${post.body}`)
+	})
+}
+getPost(posts)
+
